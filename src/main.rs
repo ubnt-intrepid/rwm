57e71b9f15fa1bab3ce1945fb5d55b5af9488644
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-01-24 18:19:58 +0900

    open/close display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate x11;
+
+#[link(name = "X11")]
+extern "C" {}
+
+use x11::xlib;
+use std::ptr::null_mut;
+
 fn main() {
-    println!("Hello, world!");
+    let display = unsafe { xlib::XOpenDisplay(null_mut()) };
+    if display == null_mut() {
+        println!("can't open display");
+        return;
+    }
+
+    unsafe {
+        xlib::XCloseDisplay(display);
+    }
 }
